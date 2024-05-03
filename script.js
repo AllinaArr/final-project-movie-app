2a9d213886fb20993104fa981083421f53125bfc
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-03 16:40:38 -0400

    css almost done

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 //getting the title and IMG_POSTER
-document.addEventListener("DOMContentLoaded", (event) => {
-  const imgPlace = document.querySelector(".grid-item");
-  fetch("http://localhost:3000/movies")
-    .then((response) => response.json())
-    .then((data) => {
-      // Iterate over the movies array
-      data.forEach((movie) => {
-        // Access the IMG_POSTER field of each movie and log it
-        console.log("Title:", movie.TITLE, ", IMG_POSTER:", movie.IMG_POSTER);
-        const imgAdd = document.createElement("img");
 
-        imgAdd.src = movie.IMG_POSTER;
-        imgAdd.alt = movie.TITLE;
-        imgPlace.append(imgAdd);
-      });
+const imgPlace = document.querySelector(".grid-container");
+fetch("http://localhost:3000/movies")
+  .then((response) => response.json())
+  .then((data) => {
+    // Iterate over the movies array
+    data.forEach((movie) => {
+      // Access the IMG_POSTER field of each movie and log it
+      console.log("Title:", movie.TITLE, ", IMG_POSTER:", movie.IMG_POSTER);
+      const imgAdd = document.createElement("img");
+      imgAdd.id = "grid-image";
+      imgAdd.src = movie.IMG_POSTER;
+      imgAdd.alt = movie.TITLE;
+      imgPlace.append(imgAdd);
     });
-});
+  });
