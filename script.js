43d21931cec4461f7f08b09c6503d61b149621c4
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-03 17:16:29 -0400

    adding comments about functions

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ fetch("http://localhost:3000/movies")
       // Access the IMG_POSTER field of each movie and log it
       console.log("Title:", movie.TITLE, ", IMG_POSTER:", movie.IMG_POSTER);
       const imgAdd = document.createElement("img");
+
+      //add EventListener about mouse and click on the img here!!
+      //I can create a function separete, but I have to call the FUNCTION HERE
       imgAdd.id = "grid-image";
       imgAdd.src = movie.IMG_POSTER;
       imgAdd.alt = movie.TITLE;
