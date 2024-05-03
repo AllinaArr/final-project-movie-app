cae5cdc286087bd2e25b715a9862d13a80275e49
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-03 14:45:03 -0400

    created my own db

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //create a fetch movies and display 10 of them on the page
-document.addEventListener("DOMContentLoaded", () => {});
+
+//https://search.imdbot.workers.dev/?q=Land/description/#IMG_POSTER -> to get a URL
+
+const movie = "Land";
+fetch(`https://search.imdbot.workers.dev/?q=${movie}/#IMG_POSTER`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // const posterUrl = data[0].poster;
+
+    // const imgElement = document.querySelector(".grid-item img");
+
+    // imgElement.src = posterUrl;
+    // imgElement.alt = data[0].title;
+  });
