f660b22a678da5f0f5aa37ca3826ab505fc1acdb
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-06 11:35:34 -0400

    updated comments

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-//getting the title and IMG_POSTER
-
 const imgPlace = document.querySelector(".grid-container");
 fetch("http://localhost:3000/movies")
   .then((response) => response.json())
   .then((data) => {
-    // Iterate over the movies array
     data.forEach((movie) => {
-      // Access the IMG_POSTER field of each movie and log it
       console.log("Title:", movie.TITLE, ", IMG_POSTER:", movie.IMG_POSTER);
       const imgAdd = document.createElement("img");
 
@@ -15,20 +11,25 @@ fetch("http://localhost:3000/movies")
       imgAdd.alt = movie.TITLE;
       imgPlace.append(imgAdd);
 
-      //add EventListener about mouse and click on the img here!!
-      //I can create a function separete, but I have to call the FUNCTION HERE
-      imgAdd.addEventListener("mouseover", handleMouseOver);
+      imgAdd.addEventListener("mouseover", () => handleMouseOver(imgAdd));
       imgAdd.addEventListener("mouseout", handleMouseOut);
     });
   });
 
-function handleMouseOver(event) {
-  event.currentTarget.style.borderColor = "gold";
-  event.currentTarget.style.border = "10px";
+function handleMouseOver(imgAdd) {
+  // event.currentTarget.style.borderColor = "gold";
   console.log("mouseoverWorked");
+  //click event and button create
+  imgAdd.addEventListener("click", () => {
+    //does not work as expected
+    const buttons = document.createElement("button");
+    buttons.textContent = "Add to watch";
+    imgAdd.appendChild(buttons);
+    console.log("Button worked");
+  });
 }
 
 function handleMouseOut(event) {
   event.currentTarget.style.borderColor = "black";
-  console.log("mouseoverWorked");
+  console.log("mouseoutWorked");
 }
